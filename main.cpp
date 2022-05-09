cdf4c649e0e623f9dbf22c3ab0e8a488eea853e2
Mariana Shashko <shashkomari@gmail.com>
2022-05-09 20:42:42 +0300

    Moveable cursor

--- main.cpp
+++ main.cpp
@@ -1,18 +1,27 @@
 #include <iostream>
+#include <ctime>
+#include <conio.h>
 using namespace std;
 
 class Field {
-private:
+protected:
     const int size_x = 10, size_y = 10;
+    const int number_of_mines = 10;
     char** playing_field = new char* [size_x];
-    
+    char** mines_field = new char* [size_x];
+    char sym = '#';
 
 public:
     Field() {
         for (int i = 0; i < size_x; i++) {
             playing_field[i] = new char[size_y];
         }
-        EmptyField();
+        for (int i = 0; i < size_x; i++) {
+            mines_field[i] = new char[size_y];
+        }
+        EmptyField(mines_field);
+        MapGeneration();
+        EmptyField(playing_field);
         Print();
     }
     ~Field() {
@@ -20,12 +29,28 @@ public:
             delete[] playing_field[i];
         }
         delete[] playing_field;
+
+        for (int i = 0; i < size_x; i++) {
+            delete[] mines_field[i];
+        }
+        delete[] mines_field;
     }
 
-    void EmptyField() {
+    void MapGeneration() {
+        for (int mines = number_of_mines; mines > 0;) {
+            int x = rand() % 10;
+            int y = rand() % 10;
+            if (mines_field[x][y] != '*') {
+                mines_field[x][y] = '*';
+                mines--;
+            }
+        }
+    }
+
+    void EmptyField(char** arr) {
         for (int i = 0; i < size_x; i++) {
             for (int j = 0; j < size_y; j++) {
-                playing_field[i][j] = '#';
+                arr[i][j] = '#';
             }
         }
     }
@@ -34,7 +59,7 @@ public:
         system("cls");
         for (int i = 0; i < size_x; i++) {
             for (int j = 0; j < size_y; j++) {
-                cout << playing_field[i][j] << ' ';
+                cout << mines_field[i][j] << ' ';
             }
             cout << endl;
         }
@@ -42,17 +67,51 @@ public:
 
     void ChangeCursorPlace(int x, int y) {
         playing_field[x][y] = 'o';
+        Print();
+    }
+
+    void SetSym(int x, int y) {
+        playing_field[x][y] = sym;
     }
     
+    void GetSym(int x, int y) {
+        sym = playing_field[x][y];
+    }
 };
 
-class Cursor {
+
+
+class Cursor: public Field {
 private:
     int current_x = 0;
     int current_y = 0;
 public:
     Cursor() {
         
+    }
+
+    void Up() {
+        if (current_x > 0) {
+            current_x--;
+        }
+    }
+
+    void Down() {
+        if (current_x < (size_x - 1)) {
+            current_x++;
+        }
+    }
+
+    void Right() {
+        if (current_y < (size_y - 1)) {
+            current_y++;
+        }
+    }
+
+    void Left() {
+        if (current_y > 0) {
+            current_y--;
+        }
     }
 
     int Update(char coordinate) {
@@ -67,9 +126,39 @@ public:
 
 int main()
 {
+    srand(time(NULL));
     Field a;
     Cursor cursor;
+    a.GetSym(cursor.Update('x'), cursor.Update('y'));
     a.ChangeCursorPlace(cursor.Update('x'), cursor.Update('y'));
-    a.Print();
+
+    while(_getch() != 27){
+        a.SetSym(cursor.Update('x'), cursor.Update('y'));
+        int ch = _getch();
+        switch (ch) {
+            case (72):
+                cursor.Up();
+                break;
+
+            case (75):
+                cursor.Left();
+                break;
+
+            case (77):
+                cursor.Right();
+                break;
+
+            case (80):
+                cursor.Down();
+                break;
+        default:
+            break;
+        }
+        a.GetSym(cursor.Update('x'), cursor.Update('y'));
+        a.ChangeCursorPlace(cursor.Update('x'), cursor.Update('y'));
+    }
+
+    system("pause");
+
     return 0;
 }
